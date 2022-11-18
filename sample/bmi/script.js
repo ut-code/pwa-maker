b5b35ce59d8fd5a184a8e3c0aa4d16c25f40b355
sg7kmr <104970808+sg7kmr@users.noreply.github.com>
2022-11-18 10:43:38 +0900

    bmiアプリ追加

---
+++ sample/bmi/script.js
@@ -0,0 +1,10 @@
+const answer = document.getElementById("answer");
+const height = document.getElementById("height");
+const weight = document.getElementById("weight");
+
+const button = document.getElementById("button");
+
+button.onclick = () => {
+  // 「button」がクリックされたときの動作
+  answer.textContent = (weight.value / (height.value / 100)) * 2;
+};
