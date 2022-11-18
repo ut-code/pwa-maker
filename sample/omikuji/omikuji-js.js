870d197222624516c34c3c19f803ea32e8d788a1
sg7kmr <104970808+sg7kmr@users.noreply.github.com>
2022-11-19 02:22:00 +0900

    全てにIDを振って管理

--- sample/omikuji/omikuji-js.js
+++ sample/omikuji/omikuji-js.js
@@ -1,33 +1,42 @@
-const omikujiButton = document.getElementById("omikuji-button");
-const omikujiImage = document.getElementById("omikuji-image");
+const omikuji_button = document.getElementById("omikuji_button"); // 「omikuji_button」の動きを操作するための準備
+const omikuji_start = document.getElementById("omikuji_start"); // 「omikuji_start」の動きを操作するための準備
+const daikichi = document.getElementById("daikichi"); // 「daikichi」の動きを操作するための準備
+const kichi = document.getElementById("kichi"); // 「kichi」の動きを操作するための準備
+const kyou = document.getElementById("kyou"); // 「kyou」の動きを操作するための準備
 
-const omikuji = document.getElementById("omikuji");
-const daikichi = document.getElementById("daikichi");
-const kichi = document.getElementById("kichi");
-const kyou = document.getElementById("kyou");
+daikichi.style.display = "none"; // 「daikichi」を隠す
+kichi.style.display = "none"; // 「kichi」を隠す
+kyou.style.display = "none"; // 「kyou」を隠す
 
-omikujiButton.onclick = () => {
-  const r = Math.random();
+omikuji_button.onclick = () => {
+  // 「omikuji_button」がクリックされたときの動作
+  const r = Math.random(); // 0以上1未満の乱数（ランダムな数）
   if (r < 0.3) {
-    omikuji.style.display = "none";
-    daikichi.style.display = "block";
+    // rが0.3より小さいときの動作
+    omikuji_start.style.display = "none"; // 「omikuji_start」を隠す
+    daikichi.style.display = "block"; // 「daikichi」を表示する
     setTimeout(() => {
-      daikichi.style.display = "none";
-      omikuji.style.display = "block";
+      // (下の数字÷1000)秒待ってから以下に書いた操作を実行する
+      daikichi.style.display = "none"; // 「daikichi」を隠す
+      omikuji_start.style.display = "block"; // 「omikuji_start」を表示する
     }, 1000);
   } else if (r < 0.7) {
-    omikuji.style.display = "none";
-    kichi.style.display = "block";
+    // rが0.3以上で、0.7より小さいときの動作
+    omikuji_start.style.display = "none"; // 「omikuji_start」を隠す
+    kichi.style.display = "block"; // 「kichi」を表示する
     setTimeout(() => {
-      kichi.style.display = "none";
-      omikuji.style.display = "block";
+      // (下の数字÷1000)秒待ってから以下に書いた操作を実行する
+      kichi.style.display = "none"; // 「kichi」を隠す
+      omikuji_start.style.display = "block"; // 「omikuji_start」を表示する
     }, 1000);
   } else {
-    omikuji.style.display = "none";
-    kyou.style.display = "block";
+    // その他（rが0.7以上）のときの動作
+    omikuji_start.style.display = "none"; // 「omikuji_start」を隠す
+    kyou.style.display = "block"; // 「kyou」を表示する
     setTimeout(() => {
-      kyou.style.display = "none";
-      omikuji.style.display = "block";
+      // (下の数字÷1000)秒待ってから以下に書いた操作を実行する
+      kyou.style.display = "none"; // 「kyou」を隠す
+      omikuji_start.style.display = "block"; // 「omikuji_start」を表示する
     }, 1000);
   }
 };
